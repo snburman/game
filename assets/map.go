28e6c649ad6b423f22783e72ce3ed73e1ebdad8f
Sean Burman <sb9443353@gmail.com>
2025-02-10 12:48:41 -0700

    Added username field to Map

--- assets/map.go
+++ assets/map.go
@@ -11,6 +11,7 @@ type Portal struct {
 type Map[T any] struct {
 	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	UserID   string             `json:"user_id" bson:"user_id"`
+	UserName string             `json:"username" bson:"username"`
 	Name     string             `json:"name" bson:"name"`
 	Primary  bool               `json:"primary" bson:"primary"`
 	Entrance struct {
